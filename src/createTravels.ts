f0097758ce5182f3cb71c1d3e2b85f9256e8ddbe
unadlib <unadlib@gmail.com>
2025-10-11 22:46:04 +0800

    fix(type): fix type

--- src/createTravels.ts
+++ src/createTravels.ts
@@ -1,6 +1,21 @@
 import type { PatchesOption, TravelsOptions } from './type';
 import { Travels } from './travels';
 
+
+/**
+ * Create a new Travels instance with auto archive mode
+ */
+export function createTravels<
+  S,
+  F extends boolean = false,
+  P extends PatchesOption = {},
+>(
+  initialState: S,
+  options?: Omit<TravelsOptions<F, true, P>, 'autoArchive'> & {
+    autoArchive?: true;
+  }
+): Travels<S, F, true, P>;
+
 /**
  * Create a new Travels instance with manual archive mode
  */
